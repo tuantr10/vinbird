209c063b3bbab185e2e4f0ee3939408194802da0
Dung <mcafee92@gmail.com>
2020-11-08 13:17:29 +0900

    about-birdnest i18n

--- about-birdnest.php
+++ about-birdnest.php
@@ -4,9 +4,9 @@
       <div class="row">
         <div class="col-md-8 ml-auto mr-auto">
           <div class="brand">
-            <h1>About Bird's Nest</h1>
-            <h3>A Nutritious Delicacy</h3>
-            <h5>Swiftlet’s nests are highly appreciated for its natural health benefit since a long time ago. Dated back from four hundred years, the edible bird’s nest is one of the most well-known Chinese delicacy as well as traditional medicine. As a nutritious food, the edible bird’s nest contains antioxidant and boosts immunity. It is also beneficial for maintaining women’s radiant beauty. </h5>
+            <h1><?php echo L::about_birdnest; ?></h1>
+            <h3><?php echo L::aboutbirdnest_title; ?></h3>
+            <h5><?php echo L::aboutbirdnest_subtitle; ?></h5>
           </div>
         </div>
       </div>
@@ -16,42 +16,35 @@
     <div class="section section-basic">
       <div class="container">
         <div class="title">
-          <h1>Edible Bird's Nest Factor</h1>
+          <h1><?php echo L::aboutbirdnest_factor_title; ?></h1>
         </div>
         <!--  buttons -->
         <div class="cd-section">
           <div class="row">
               <div class="col-md-8 ml-auto mr-auto">
-                <p>
-                    There are many effects that have been known: Enhancing lung health, beautiful skin, stimulating cell growth and rejuvenation, increased red blood cells and hemoglobin.  
-                </p>
-
-                <p>
-                    Swallow Nest has a very good body-building effect, ranked at the top position in "Bat Tran" of precious foods.  Ancient swallow nest cleanses the lungs and respiratory organs, reduces allergy symptoms, increases body weight, balances metabolic processes in the body, enhances the ability of physical and anti-inflammatory activities.  
-                </p>
-
-                <p>
-                    Neurological radiation, enhances resistance, immunity, very hematological system, increases the number of red blood cells, hemoglobin, reduces blood clotting, enhances the growth stimulation for cells, restores cells  hurt, anti-aging, rejuvenating life.  
-                </p>
+                <p><?php echo L::aboutbirdnest_factor_title_description_1; ?></p>
+                <p><?php echo L::aboutbirdnest_factor_title_description_2; ?></p>
+                <p><?php echo L::aboutbirdnest_factor_title_description_3; ?></p>
               </div>
           </div>
           <div class="row">
               <div class="col-md-6 ml-auto mr-auto">
-                  <h3>NO DETERGENT</h3>
-                  During the cleaning process we do not use any cleaning or cleaning chemicals.  We use saddle washing water which is certified by Pasteur Institute (because chlorine in tap water can degrade bird's nest).  Because it is difficult and time-consuming to clean the bird's nest by hand, some establishments can use the cleaning agent, although it is not toxic, but it can reduce the nutrition of bird's nest.             </div>
+                <h3><?php echo L::aboutbirdnest_factor_1_title; ?></h3>
+                <?php echo L::aboutbirdnest_factor_1_description; ?>
+              </div>
               <div class="col-md-6 ml-auto mr-auto">
-                  <h3>NO STAINING</h3>
-                  Natural nests can have pure white, ivory, orange, yellow, brown, red.  .  .  bleaching or staining according to consumer tastes may impair the nutritional value of the nest.  
+                <h3><?php echo L::aboutbirdnest_factor_2_title; ?></h3>
+                <?php echo L::aboutbirdnest_factor_2_description; ?>
               </div>
             </div>
             <div class="row">
               <div class="col-md-6 ml-auto mr-auto">
-                  <h3>NO ADDITIVES</h3>
-                  Natural oats are lightweight, so people can use fiber or other foods to fill in to increase weight, some consumers prefer crispy, tough oats.  .  .  You can use borax.  All of these substances degenerate the swallow's nest.  We absolutely do not use any additives. 
+                <h3><?php echo L::aboutbirdnest_factor_3_title; ?></h3>
+                <?php echo L::aboutbirdnest_factor_3_description; ?>
               </div>
               <div class="col-md-6 ml-auto mr-auto">
-                  <h3>NO PRESERVATIVES</h3>
-                   Pure bird's nest can last for a long time in the natural environment, but if there are fillers or additives to increase weight, color, smell, you must use more preservatives, Vinbirdnest  Absolutely no preservatives used.
+                <h3><?php echo L::aboutbirdnest_factor_4_title; ?></h3>
+                <?php echo L::aboutbirdnest_factor_4_description; ?>
               </div>
             </div>
         </div>
@@ -62,40 +55,40 @@
             <div class="row">
                 <div class="col-md-8 ml-auto mr-auto">
                     <div class="brand">
-                        <h1 class="text-center">Natural Benefit</h1>
-                        <h5>Edible’s bird nest contains nutritions which help us to improve health and well-being.</h5>
+                        <h1 class="text-center"><?php echo L::aboutbirdnest_benefit_title; ?></h1>
+                        <h5><?php echo L::aboutbirdnest_benefit_description; ?></h5>
                     </div>
                 </div>
             </div>
             <div class="row">
                 <div class="col-md-3 ml-auto mr-auto card">
                     <div class="card-header card-header-primary">
-                        <div class="text-center">SIALIC ACID</div>
+                        <div class="text-center"><?php echo L::aboutbirdnest_benefit_1_title; ?></div>
                     </div>
                     <div class="card-body">
                         <img class="rounded-circle img-fluid" src="https://images.squarespace-cdn.com/content/v1/598005bc1e5b6c1c9fa13515/1517221956355-GW4JI9VND08FC03OTIFR/ke17ZwdGBToddI8pDm48kP06O0_IHyRXSOOiqwgWaApZw-zPPgdn4jUwVcJE1ZvWEtT5uBSRWt4vQZAgTJucoTqqXjS3CfNDSuuf31e0tVEsL0EX72Q6S7TgfQYQBQpkz5xM6Qt8VXd_xJGg_ziCFib8BodarTVrzIWCp72ioWw/Zat+BN-sialic.png?format=750w">
                         <div class="space-50"></div>
-                        Vinbirdnest edible bird’s nest is a natural source of sialic acid, which has special role for both our body and brain.  It boosts up our over-all immunity, improves learning ability, and helps prevent influenza. The sialic acid level in our bird’s nest is closely monitored by our team of researchers regularly.
+                        <?php echo L::aboutbirdnest_benefit_1_description; ?>
                     </div>
                 </div>
                 <div class="col-md-3 ml-auto mr-auto card">
                     <div class="card-header card-header-primary">
-                        <div class="text-center">GLYCOPROTEIN</div>
+                        <div class="text-center"><?php echo L::aboutbirdnest_benefit_2_title; ?></div>
                     </div>
                     <div class="card-body">
                         <img class="rounded-circle img-fluid" src="https://images.squarespace-cdn.com/content/v1/598005bc1e5b6c1c9fa13515/1517222112423-OCAA3AXZ1PPG5B8ONLMP/ke17ZwdGBToddI8pDm48kP06O0_IHyRXSOOiqwgWaApZw-zPPgdn4jUwVcJE1ZvWEtT5uBSRWt4vQZAgTJucoTqqXjS3CfNDSuuf31e0tVEsL0EX72Q6S7TgfQYQBQpkz5xM6Qt8VXd_xJGg_ziCFib8BodarTVrzIWCp72ioWw/Zat+BN-protein.png?format=750w">
                         <div class="space-50"></div>
-                        Glycoprotein has various function; maintaining our immune system, blood, digestion, lungs, intercellular communication, reproduction system, and skin health. Proteins contained in edible bird’s nests also have active compounds such as amino acids that help to build muscle protein, produce antibodies, and repair damaged tissues.
+                        <?php echo L::aboutbirdnest_benefit_2_description; ?>
                     </div>
                 </div>
                 <div class="col-md-3 ml-auto mr-auto card">
                     <div class="card-header card-header-primary">
-                        <div class="text-center">MINERAL</div>
+                        <div class="text-center"><?php echo L::aboutbirdnest_benefit_3_title; ?></div>
                     </div>
                     <div class="card-body">
                         <img class="rounded-circle img-fluid" src="https://images.squarespace-cdn.com/content/v1/598005bc1e5b6c1c9fa13515/1517228028927-8NNV5CYZ1AQ3NQIXNV7Z/ke17ZwdGBToddI8pDm48kP06O0_IHyRXSOOiqwgWaApZw-zPPgdn4jUwVcJE1ZvWEtT5uBSRWt4vQZAgTJucoTqqXjS3CfNDSuuf31e0tVEsL0EX72Q6S7TgfQYQBQpkz5xM6Qt8VXd_xJGg_ziCFib8BodarTVrzIWCp72ioWw/Zat+BN-mineral.png?format=750w">
                         <div class="space-50"></div>
-                        Packed with several kind of minerals, edible bird’s nest is a perfect super food for our body. Mineral has various benefit for health, such as cell regeneration, strengthening immune system as well as body growth & development.
+                        <?php echo L::aboutbirdnest_benefit_3_description; ?>
                     </div>
                 </div>
             </div>
